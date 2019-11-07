784b19b748ca04f46d51ddd55b11e8bd5c8a5908
Дмитрий Кречетников <dkrech07@gmail.com>
2019-11-07 18:57:28 +0500

    Вносит исправления

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ var PHOTOS_NUMBER = 25;
 var MAX_LENGTH_COMMENT = 2;
 var MIN_LIKES = 15;
 var MAX_LIKES = 200;
+var MAX_COMMENTS = 10;
 
 var COMMENTS_LIST = [
   'Всё отлично!',
@@ -14,6 +15,17 @@ var COMMENTS_LIST = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
+var DESCRIPTION_LIST = [
+  'Тестим новую камеру!',
+  ' Затусили с друзьями на море',
+  'Как же круто тут кормят',
+  'Отдыхаем...',
+  'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......',
+  'Вот это тачка!'
+];
+
+var NAMES_LIST = ['Татьяна', 'Николай', 'Снежанна', 'Ирина', 'Анжела', 'Игорь', 'Карась'];
+
 var getRandom = function (number) {
   return Math.floor(Math.random() * number);
 };
@@ -22,28 +34,60 @@ var getRandomDouble = function (min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-var getRandomComment = function () {
-  var compareRandom = function () {
-    return Math.random() - 0.5;
-  };
+var compareRandom = function () {
+  return Math.random() - 0.5;
+};
 
+var getRandomDescription = function () {
+  var description = DESCRIPTION_LIST[getRandom(DESCRIPTION_LIST.length)];
+  return description;
+};
+
+var getRandomLikes = function () {
+  var likes = getRandomDouble(MIN_LIKES, MAX_LIKES);
+  return likes;
+};
+
+var getRandomMessage = function () {
   var newArray = Array.from(COMMENTS_LIST);
-  var comment = newArray.sort(compareRandom);
+  var message = newArray.sort(compareRandom);
+  message.length = getRandom(MAX_LENGTH_COMMENT) + 1;
 
-  comment.length = getRandom(MAX_LENGTH_COMMENT) + 1;
+  return message.join('", "');
+};
+
+var getRandomName = function () {
+  var name = NAMES_LIST[getRandom(NAMES_LIST.length)];
+  return name;
+};
+
+var getRandomComment = function () {
+  var comment = {
+    avatar: 'img/avatar-' + getRandom(PHOTOS_NUMBER) + '.svg',
+    message: getRandomMessage(),
+    name: getRandomName()
+  };
 
   return comment;
 };
 
+var getAllCommenst = function () {
+  var commentArray = [];
+  for (var i = 0; i < getRandom(MAX_COMMENTS) + 1; i++) {
+    commentArray.push(getRandomComment());
+  }
+
+  return commentArray;
+};
 
 var getPhotosArray = function () {
   var photosArray = [];
   for (var i = 0; i < PHOTOS_NUMBER; i++) {
     var photo = {
       url: 'photos/' + (i + 1) + '.jpg',
-      description: '',
-      likes: getRandomDouble(MIN_LIKES, MAX_LIKES),
-      comments: getRandomComment()
+      description: getRandomDescription(),
+      likes: getRandomLikes(),
+      comments: getAllCommenst()
     };
     photosArray.push(photo);
   }
@@ -76,5 +120,4 @@ var drawPhotos = function () {
   pictures.appendChild(fragment);
 };
 
-console.log(getPhotosArray());
 drawPhotos();
