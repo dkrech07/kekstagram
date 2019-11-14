1d9922effd48d8650285a8156450e538a497f20d
Дмитрий Кречетников <dkrech07@gmail.com>
2019-11-15 02:30:19 +0500

    Выносит код в gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,64 @@
 'use strict';
 // gallery.js — модуль, который работает с галереей изображений; Использует вспомогательные модули:
 (function () {
+  window.ESC_KEYCODE = 27;
+
+  var ENTER_KEYCODE = 13;
+
+  var closeBigPictureButton = window.preview.bigPicture.querySelector('.big-picture__cancel');
+  var photosContainer = document.querySelector('.pictures');
+
+  var markPhotos = function () {
+    var allPhotos = photosContainer.querySelectorAll('.picture');
+    for (var i = 0; i < allPhotos.length; i++) {
+      allPhotos[i].id = i;
+    }
+  };
+
+  var removeBigPictureListeners = function () {
+    closeBigPictureButton.removeEventListener('click', bigPictureCloseHandler);
+    document.removeEventListener('keydown', bigPictureEscHandler);
+  };
+
+  var bigPictureCloseHandler = function () {
+    window.preview.bigPicture.classList.add('hidden');
+    removeBigPictureListeners();
+  };
+
+  var bigPictureEscHandler = function (evt) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      bigPictureCloseHandler();
+    }
+  };
+
+  var addPhotoHandle = function (photoId) {
+    if (photoId || photoId === 0) {
+      window.preview.drawBigPicture(photoId);
+
+      closeBigPictureButton.addEventListener('click', bigPictureCloseHandler);
+      document.addEventListener('keydown', bigPictureEscHandler);
+    }
+  };
+
+  var photoClickHandler = function (evt) {
+    var target = evt.target;
+    var photoId = parseInt(target.parentElement.id, 10);
+
+    addPhotoHandle(photoId);
+  };
+
+  var photoEnterHandler = function (evt) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+      var target = evt.target;
+      var photoId = parseInt(target.id, 10);
+
+      addPhotoHandle(photoId);
+    }
+  };
+
+  photosContainer.addEventListener('click', photoClickHandler);
+  photosContainer.addEventListener('keydown', photoEnterHandler);
+
   window.drawPhotos();
+  markPhotos();
 })();
--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 'use strict';
 
-
 var MAX_COMMENT_LENGTH = 140;
-var ESC_KEYCODE = 27;
-var ENTER_KEYCODE = 13;
+
 var MAX_EFFECT_LEVEL = 100;
 var FOBOS_EFFECT_LEVEL = 25;
 var HEAT_EFFECT_LEVEL = 33;
@@ -12,10 +10,6 @@ var MAX_SCALE = 100;
 var ALERT_COLOR = '#FF4E4E';
 var HASHTAGS_NUMBER = 5;
 var HASHTAG_LENGTH = 20;
-
-var bigPicture = document.querySelector('.big-picture');
-var closeBigPictureButton = bigPicture.querySelector('.big-picture__cancel');
-var photosContainer = document.querySelector('.pictures');
 
 var uploadButton = document.querySelector('#upload-file');
 var imageForm = document.querySelector('.img-upload__overlay');
@@ -34,61 +28,6 @@ var hashtagsInput = uploadForm.querySelector('.text__hashtags');
 var formCommentInput = uploadForm.querySelector('.text__description');
 var hashtagsArray = [];
 var hashtagsErrorMessage = '';
-
-var removeChild = function (element) {
-  while (element.firstChild) {
-    element.removeChild(element.firstChild);
-  }
-};
-
-var drawPhotoInformation = function (number, element) {
-  var image = bigPicture.querySelector('.big-picture__img img');
-  image.src = element.url;
-
-  var likesCount = bigPicture.querySelector('.likes-count');
-  likesCount.textContent = element.likes;
-
-  var commentsCount = bigPicture.querySelector('.comments-count');
-  commentsCount.textContent = element.comments.length;
-
-  var descriptionPhoto = bigPicture.querySelector('.social__caption');
-  descriptionPhoto.textContent = element.description;
-};
-
-var drawCommentInformation = function (number, element) {
-  var socialComments = bigPicture.querySelector('.social__comments');
-  var template = socialComments.querySelector('.social__comment');
-  var fragment = document.createDocumentFragment();
-
-  removeChild(socialComments);
-  for (var i = 0; i < element.comments.length; i++) {
-    var comment = template.cloneNode(true);
-    var commentPhoto = comment.querySelector('.social__picture');
-    var commentMessage = comment.querySelector('.social__text');
-    commentPhoto.src = element.comments[i].avatar;
-    commentPhoto.alt = element.comments[i].name;
-    commentMessage.textContent = element.comments[i].message;
-    fragment.appendChild(comment);
-  }
-  socialComments.appendChild(fragment);
-};
-
-var hideBigPhotoElements = function () {
-  var commentsCounter = bigPicture.querySelector('.social__comment-count');
-  var commentsLoader = bigPicture.querySelector('.comments-loader');
-  commentsCounter.classList.add('visually-hidden');
-  commentsLoader.classList.add('visually-hidden');
-};
-
-var drawBigPicture = function (number) {
-  bigPicture.classList.remove('hidden');
-  var element = window.data.getPhotosArray()[number];
-
-  drawPhotoInformation(number, element);
-  drawCommentInformation(number, element);
-
-  hideBigPhotoElements();
-};
 
 var getEffectDefault = function () {
   effectLevelPin.style.left = MAX_EFFECT_LEVEL + '%';
@@ -277,7 +216,7 @@ var removeCommentHandlers = function () {
 };
 
 var closeFormEscHandler = function (evt) {
-  if (evt.keyCode === ESC_KEYCODE) {
+  if (evt.keyCode === window.ESC_KEYCODE) {
     removeChangeHandler();
   }
 };
@@ -310,56 +249,3 @@ var uploadChangeHandler = function () {
 getEffectDefault();
 effectLevelSlider.classList.add('hidden');
 uploadButton.addEventListener('change', uploadChangeHandler);
-
-var markPhotos = function () {
-  var allPhotos = photosContainer.querySelectorAll('.picture');
-  for (var i = 0; i < allPhotos.length; i++) {
-    allPhotos[i].id = i;
-  }
-};
-
-markPhotos();
-
-var bigPictureCloseHandler = function () {
-  bigPicture.classList.add('hidden');
-  removeBigPictureListeners();
-};
-
-var bigPictureEscHandler = function (evt) {
-  if (evt.keyCode === ESC_KEYCODE) {
-    bigPictureCloseHandler();
-  }
-};
-
-var removeBigPictureListeners = function () {
-  closeBigPictureButton.removeEventListener('click', bigPictureCloseHandler);
-  document.removeEventListener('keydown', bigPictureEscHandler);
-};
-
-var addPhotoHandle = function (photoId) {
-  if (photoId || photoId === 0) {
-    drawBigPicture(photoId);
-
-    closeBigPictureButton.addEventListener('click', bigPictureCloseHandler);
-    document.addEventListener('keydown', bigPictureEscHandler);
-  }
-};
-
-var photoClickHandler = function (evt) {
-  var target = evt.target;
-  var photoId = parseInt(target.parentElement.id, 10);
-
-  addPhotoHandle(photoId);
-};
-
-var photoEnterHandler = function (evt) {
-  if (evt.keyCode === ENTER_KEYCODE) {
-    var target = evt.target;
-    var photoId = parseInt(target.id, 10);
-
-    addPhotoHandle(photoId);
-  }
-};
-
-photosContainer.addEventListener('click', photoClickHandler);
-photosContainer.addEventListener('keydown', photoEnterHandler);
